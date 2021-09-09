fca3ee082fafb67e0eafbd5c468d1199683e8260
Michael Richardson <michael@richardsons.co.za>
2021-09-09 21:50:46 +0100

    buffer size to int

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = 1e5       # replay buffer size
+BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
